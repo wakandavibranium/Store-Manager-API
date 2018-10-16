ff30eb2dea4cab105f448e16c563b5770b1e1aca
unknown <kamaumike2013@gmail.com>
2018-10-17 01:34:38 +0300

    [Feature #161202210] Register 'v1' blueprint and remove trailing slash in url

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,17 @@ def create_app(config_name):
     """Create an instance of Flask and load with environment variables"""
 
     app = Flask(__name__, instance_relative_config=True)
+    
+    #Remove trailing slash in url
+    app.url_map.strict_slashes = False
+
+    #Import blueprint we created
+    from .api.v1 import app_version1
+
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    
+    #Register blueprint we created
+    app.register_blueprint(app_version1)
 
     return app
