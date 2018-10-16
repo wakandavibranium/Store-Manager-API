184ac5b833ba3be5e001152ed5259886808e778e
unknown <kamaumike2013@gmail.com>
2018-10-16 22:30:05 +0300

    [Chore #161203092] Add fail test for get all products

---
+++ app/tests/v1/test_products.py
@@ -0,0 +1,33 @@
+import unittest
+
+from ... import create_app
+
+
+class ProductTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """Initialize Flask app and declare variables to
+        be used before running every test"""
+
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client()
+        self.content_type = "application/json"
+        self.product = {
+            'name': 'Popcorn',
+            'category': 'Snacks',
+            'quantity': 150,
+            "m_q": 5,
+            'price': 20}
+
+    def tearDown(self):
+        """Empty the product dictionary after running every test"""
+
+        self.product = {}
+
+    def test_user_can_get_all_products(self):
+        """Test user can get all products"""
+
+        response = self.client.get(
+            "/api/v1/products",
+            content_type=self.content_type)
+        self.assertEqual(response.status_code, 200)
