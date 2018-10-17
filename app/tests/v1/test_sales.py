8d4082c91ceaf757af6f6ff69258f2b1cf3e66fb
unknown <kamaumike2013@gmail.com>
2018-10-17 16:27:17 +0300

    [Chore #161203128] Add fail test to get all sales records

---
+++ app/tests/v1/test_sales.py
@@ -0,0 +1,34 @@
+import unittest
+from datetime import datetime
+
+# Local import
+from ... import create_app
+
+
+class SaleTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """Initialize Flask app and declare variables to
+        be used before running every test"""
+
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client()
+        self.content_type = "application/json"
+        self.sale = {
+            'number_of_items_sold': 12,
+            'transaction_amount': 34000,
+            'date_created': datetime.now,
+            "created_by": "John Doe"}
+
+    def tearDown(self):
+        """Empty the sale dictionary after running every test"""
+
+        self.sale = {}
+
+    def test_admin_can_get_all_sales_records(self):
+        """Test admin can get all sales"""
+
+        response = self.client.get(
+            "/api/v1/sales",
+            content_type=self.content_type)
+        self.assertEqual(response.status_code, 200)
