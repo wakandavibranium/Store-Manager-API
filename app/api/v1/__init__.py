c9611957532432fae3a7a07c1182763cce73834c
unknown <kamaumike2013@gmail.com>
2018-10-17 17:25:06 +0300

    [Feature #161202776] Add 'sales' namespace

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
 from flask_restplus import Api
 
-#Local import
+# Local import
 from .views import namespace_1 as n1
+from .views import namespace_2 as n2
 
-#create a blueprint
+# create a blueprint
 app_version1 = Blueprint("v1", __name__, url_prefix="/api/v1")
 api_version1 = Api(app_version1)
-api_version1.add_namespace(n1,path='/products')
+
+# Add namespaces
+api_version1.add_namespace(n1, path='/products')
+api_version1.add_namespace(n2, path='/sales')
