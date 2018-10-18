92a161de23006dcada64d48116dfe843ec805c0c
unknown <kamaumike2013@gmail.com>
2018-10-18 10:55:26 +0300

    [Chore #161202921] Add pep8 styling

--- instance/config.py
+++ instance/config.py
@@ -1,29 +1,30 @@
 import os
 
+
 class Config(object):
     """Common configurations"""
     CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
 class DevelopmentConfig(Config):
     """Configurations for Development"""
     DEBUG = True
 
+
 class ProductionConfig(Config):
     """Configurations for Production."""
     DEBUG = False
     TESTING = False
 
+
 class TestingConfig(Config):
-    """Configurations for Testing, with a separate test database."""
+    """Configurations for Testing"""
     TESTING = True
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/test_db'
-    DEBUG = True   
+    DEBUG = True
 
 
 app_config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
-    'testing': TestingConfig,    
+    'testing': TestingConfig,
 }
