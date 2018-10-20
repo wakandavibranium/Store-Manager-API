74453f0e8cf0cc5e4c0740cf45bc0681fe07ad95
unknown <kamaumike2013@gmail.com>
2018-10-20 11:04:29 +0300

    [Feature #161360416] Add namespace for signup

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,6 +4,7 @@ from flask_restplus import Api
 # Local import
 from .views import namespace_1 as n1
 from .views import namespace_2 as n2
+from .views import namespace_3 as n3
 
 # create a blueprint
 app_version1 = Blueprint("v1", __name__, url_prefix="/api/v1")
@@ -12,3 +13,4 @@ api_version1 = Api(app_version1)
 # Add namespaces
 api_version1.add_namespace(n1, path='/products')
 api_version1.add_namespace(n2, path='/sales')
+api_version1.add_namespace(n3, path='/auth/signup')
